4a89928000d12ffd5307e053a08390823880f838
David van der Spoel <david.vanderspoel@icm.uu.se>
2022-08-17 19:38:29 +0200

    Updated

--- melt_continue.py
+++ melt_continue.py
@@ -21,8 +21,9 @@ def copy_ifnot_exists(src:str, dst:str):
     if not os.path.exists(dst):
         shutil.copy(src, dst)
         
-def run_melt(mol:str, temp:float, prev:dict):
+def run_melt(mol:str, temp:float, prev:dict, nsteps:int):
     if not ((prev["host"] == "csb" and csb) or (prev["host"] != "csb" and not csb)):
+        print("Cannot continue this simulation on this host since it was run on %s earlier" % prev["host"])
         return
     meltroot = "melting"
     os.makedirs(meltroot, exist_ok=True)
@@ -36,17 +37,46 @@ def run_melt(mol:str, temp:float, prev:dict):
     target  = prev["trrfile"][:-4]
     for fn in [ "cptfile", "edrfile", "trrfile", "tprfile", "tprfile", "logfile" ]:
         copy_ifnot_exists(basedir+prev[fn], prev[fn])
-    if not os.path.exists(target + ".sh"):
-        write_run_job(target, 10000000)
+    write_run_job(target, nsteps)
     os.chdir("../../..")
+    
+def parseArguments():
+    parser = argparse.ArgumentParser(
+      prog='simtable.py',
+      description=
+"""
+Script to continue melting simulations.
+""")
+    mtname = "melt_table.csv" 
+    parser.add_argument("-mol", "--molecule", help="The molecule to run", type=str, default=None)
+    parser.add_argument("-temp", "--temperature", nargs='+', help="Temperature(s) to simulate at", type=float, default=0)
+    nsteps = 10000000
+    parser.add_argument("-nsteps", "--nsteps", help="Number of steps to extend simulation with, default "+str(nsteps), type=int, default=nsteps)
+    parser.add_argument("-v", "--verbose", help="Print debugging info", action="store_true")
+    return parser.parse_args()
     
 if __name__ == '__main__':
     if shutil.which("gmx") == None:
         sys.exit("Please load the gromacs environment using\nml load GCC/10.2.0  OpenMPI/4.0.5 GROMACS/2021")
+    args  = parseArguments()
+    if args.verbose:
+        Debug = True
 
     simtable = get_simtable("simtable.csv")
-    
-    for mol in [ "furan" ]:
-        for temp in ignore[mol]:
-            run_melt(mol, temp, simtable[mol][temp])
 
+    if None == args.molecule:
+        print("Please provide a molecule name (use -h flag to get info)")
+    elif args.nsteps <= 0:
+        print("Specify a positive number of steps")
+    elif args.molecule not in simtable:
+        print("Molecule %s unknown. Pick one of:\n" % args.molecule)
+        print(simtable.keys())
+    else:
+        for temp in args.temperature:
+            if temp not in simtable[args.molecule]:
+                print("No previous simulation at temperature %g. Try one of these:\n" % temp)
+                print(sorted(simtable[args.molecule].keys()))
+            elif simtable[args.molecule][temp]["host"].find("csb") >= 0:
+                print("Can only continue simulations on kebnekaise")
+            else:
+                run_melt(args.molecule, temp, simtable[args.molecule][temp], args.nsteps)
