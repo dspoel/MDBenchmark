011926e241f1b13a146481339450da55be6b5ae3
David van der Spoel <david.vanderspoel@icm.uu.se>
2022-08-28 13:38:19 +0200

    Updated

--- simtable.py
+++ simtable.py
@@ -17,12 +17,11 @@ def get_simtable(filenames:list) -> dict:
                     simtable[row[0]] = {}
                 temp    = int(row[1])
                 trrtime = float(row[12])
-                if (not temp in simtable[row[0]] or trrtime > simtable[row[0]][temp]["trrtime"] or 
-                    (length == simtable[row[0]][temp]["length"] and (row[3].find("keb") >= 0) or (row[3].find("csb2") >= 0))):
-                    simtable[row[0]][temp] = { "host": row[3], "length": length, "nmol": int(row[4]),
+                if (not temp in simtable[row[0]] or trrtime > simtable[row[0]][temp]["trrtime"]):
+                    simtable[row[0]][temp] = { "host": row[3], "length": float(row[2]), "nmol": int(row[4]),
                                                "simdir": row[5], "logfile": row[6], "cptfile": row[7],
                                                "tprfile": row[8], "trrfile": row[9], "edrfile": row[10],
-                                               "grofile": row[11], "trrtime": trrtime) }
+                                               "grofile": row[11], "trrtime": trrtime }
     return simtable
 
 def dump_simtable(simtable:dict, filename:str):
