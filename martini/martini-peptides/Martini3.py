97679b3bc40baca0fb689128e2eb48100301f3d8
najla23 <najla.hosseini@gmail.com>
2023-12-20 19:36:36 +0100

    script

--- martini/martini-peptides/Martini3.py
+++ martini/martini-peptides/Martini3.py
@@ -29,6 +29,9 @@ for maindir in glob.glob("M*"):
 
         with open("../../../MDP/peptides/NPTP.mdp", "r") as original:
              original_c = original.read()
+
+        with open("../../../MDP/peptides/NPTB.mdp", "r") as original2:
+             original2_c = original2.read()     
         
             
         with open("martini.sh", "w") as script:
@@ -43,28 +46,28 @@ echo -e "{original_c}\nref_t= {temperature}" > NPTP.mdp
 cp ../../../MDP/peptides/EM.mdp .
 cp ../../../MDP/peptides/EM100.mdp .
 cp ../../../MDP/peptides/NPT100P.mdp .
+cp ../../../MDP/peptides/NPT100P2.mdp .
+cp ../../../MDP/peptides/NPTP2.mdp .
 
-gmx grompp  -c boxxx.pdb -f EM100.mdp -o EM100.tpr -r boxxx.pdb -p topol.top
+gmx grompp  -c boxxx.pdb -f EM100.mdp -o EM100.tpr -r boxxx.pdb -p topol.top -maxwarn 1
 gmx mdrun -nt 16 -s EM100.tpr -deffnm EM100 -c EM100.gro
-echo -e "a  bb\nq" | gmx make_ndx -f EM.gro -o index.ndx
-echo -e "bb"|gmx genrestr -f EM.gro -n index.ndx -o posre.itp -fc 1000 1000 1000
-subprocess.run(f'awk -F \'\\\\\' \'{{print "#ifdef POSRES\\n#include \\"posre.itp\\"\\n#endif"}}1\' topol.top > topol_new.top', shell=True)
+echo -e "a  bb\nq" | gmx make_ndx -f EM100.gro -o index.ndx
+echo -e "bb"|gmx genrestr -f EM100.gro -n index.ndx -o posre.itp -fc 1000 1000 1000
 
-gmx grompp  -c EM100.gro -f NPT100p.mdp -o NPT100_new.tpr -r EM100.gro -p topol_new.top -maxwarn 1
+sed "/\[ system \]/s/.*/#ifdef POSRES\\n#include \"posre.itp\"\\n#endif\\n&/" topol.top > topol_new.top
+
+gmx grompp  -c EM100.gro -f NPT100P2.mdp -o NPT100_new.tpr -r EM100.gro -p topol_new.top -maxwarn 1
 gmx mdrun -nt 16 -s NPT100_new.tpr -deffnm NPT100_new -c NPT100_new.gro
 
-gmx grompp  -c NPT100_new.gro -f NPT100p.mdp -o NPT100.tpr -r NPT100_new.gro -p topol.top -maxwarn 1
+gmx grompp  -c NPT100_new.gro -f NPT100P.mdp -o NPT100.tpr -r NPT100_new.gro -p topol.top -maxwarn 1
 gmx mdrun -nt 16 -s NPT100.tpr -deffnm NPT100 -c NPT100.gro
 
 
 
-gmx grompp  -c boxxx.pdb -f EM.mdp -o EM.tpr -r boxxx.pdb -p topol.top
+gmx grompp  -c boxxx.pdb -f EM.mdp -o EM.tpr -r boxxx.pdb -p topol.top -maxwarn 1
 gmx mdrun -nt 16 -s EM.tpr -deffnm EM -c EM.gro
-echo -e "a  bb\nq" | gmx make_ndx -f EM.gro -o index.ndx
-echo -e "bb"|gmx genrestr -f EM.gro -n index.ndx -o posre.itp -fc 1000 1000 1000
-subprocess.run(f'awk -F \'\\\\\' \'{{print "#ifdef POSRES\\n#include \\"posre.itp\\"\\n#endif"}}1\' topol.top > topol_new.top', shell=True)
 
-gmx grompp  -c EM.gro -f NPTP.mdp -o NPT_new.tpr -r EM.gro -p topol_new.top -maxwarn 1
+gmx grompp  -c EM.gro -f NPTP2.mdp -o NPT_new.tpr -r EM.gro -p topol_new.top -maxwarn 1
 gmx mdrun -nt 16 -s NPT_new.tpr -deffnm NPT_new -c NPT_new.gro
 
 gmx grompp  -c NPT_new.gro -f NPTP.mdp -o NPT.tpr -r NPT_new.gro -p topol.top -maxwarn 1
@@ -73,30 +76,27 @@ gmx mdrun -nt 16 -s NPT.tpr -deffnm NPT -c NPT.gro
 mkdir BR
 cd BR
 cp ../../../../MDP/peptides/NPTB.mdp .
-echo -e "{original_c}\nref_t= {temperature}" > NPTB.mdp
+echo -e "{original2_c}\nref_t= {temperature}" > NPTB.mdp
 cp ../../../../MDP/peptides/EM.mdp .
 cp ../../../../MDP/peptides/EM100.mdp .
 cp ../../../../MDP/peptides/NPTB100.mdp .
+cp ../../../../MDP/peptides/NPTB1002.mdp .
+cp ../../../../MDP/peptides/NPTB2.mdp .
 
-gmx grompp  -c ../boxxx.pdb -f EM100.mdp -o EM100.tpr -r ../boxxx.pdb -p ../topol.top
+gmx grompp  -c ../boxxx.pdb -f EM100.mdp -o EM100.tpr -r ../boxxx.pdb -p ../topol.top -maxwarn 1
 gmx mdrun -nt 16 -s EM100.tpr -deffnm EM100 -c EM100.gro
-echo -e "a  bb\nq" | gmx make_ndx -f EM.gro -o index.ndx
-echo -e "bb"|gmx genrestr -f EM.gro -n index.ndx -o posre.itp -fc 1000 1000 1000
-subprocess.run(f'awk -F \'\\\\\' \'{{print "#ifdef POSRES\\n#include \\"posre.itp\\"\\n#endif"}}1\' topol.top > topol_new.top', shell=True)
 
-gmx grompp  -c EM100.gro -f NPTB100.mdp -o NPT100_new.tpr -r EM100.gro -p ../topol_new.top -maxwarn 1
+
+gmx grompp  -c EM100.gro -f NPTB1002.mdp -o NPT100_new.tpr -r EM100.gro -p ../topol_new.top -maxwarn 1
 gmx mdrun -nt 16 -s NPT100_new.tpr -deffnm NPT100_new -c NPT100_new.gro
 
 gmx grompp  -c NPT100_new.gro -f NPTB100.mdp -o NPT100.tpr -r NPT100_new.gro -p ../topol.top -maxwarn 1
 gmx mdrun -nt 16 -s NPT100.tpr -deffnm NPT100 -c NPT100.gro
 
-gmx grompp  -c ../boxxx.pdb -f EM.mdp -o EM.tpr -r ../boxxx.pdb -p ../topol.top
+gmx grompp  -c ../boxxx.pdb -f EM.mdp -o EM.tpr -r ../boxxx.pdb -p ../topol.top -maxwarn 1
 gmx mdrun -nt 16 -s EM.tpr -deffnm EM -c EM.gro
-echo -e "a  bb\nq" | gmx make_ndx -f EM.gro -o index.ndx
-echo -e "bb"|gmx genrestr -f EM.gro -n index.ndx -o posre.itp -fc 1000 1000 1000
-subprocess.run(f'awk -F \'\\\\\' \'{{print "#ifdef POSRES\\n#include \\"posre.itp\\"\\n#endif"}}1\' topol.top > topol_new.top', shell=True)
 
-gmx grompp  -c EM.gro -f NPTB.mdp -o NPT_new.tpr -r EM.gro -p ../topol_new.top -maxwarn 1
+gmx grompp  -c EM.gro -f NPTB2.mdp -o NPT_new.tpr -r EM.gro -p ../topol_new.top -maxwarn 1
 gmx mdrun -nt 16 -s NPT_new.tpr -deffnm NPT_new -c NPT_new.gro
 
 gmx grompp  -c NPT_new.gro -f NPTB.mdp -o NPT.tpr -r NPT_new.gro -p ../topol.top -maxwarn 1
