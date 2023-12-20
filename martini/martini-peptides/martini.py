e28f7df4e1940a730112419778992a33a3c398a9
najla23 <najla.hosseini@gmail.com>
2023-12-20 07:38:01 +0100

    script

--- martini/martini-peptides/martini.py
+++ martini/martini-peptides/martini.py
@@ -46,12 +46,28 @@ cp ../../../MDP/peptides/NPT100P.mdp .
 
 gmx grompp  -c boxxx.pdb -f EM100.mdp -o EM100.tpr -r boxxx.pdb -p topol.top
 gmx mdrun -nt 16 -s EM100.tpr -deffnm EM100 -c EM100.gro
-gmx grompp  -c EM100.gro -f NPT100p.mdp -o NPT100.tpr -r EM100.gro -p topol.top
+echo -e "a  bb\nq" | gmx make_ndx -f EM.gro -o index.ndx
+echo -e "bb"|gmx genrestr -f EM.gro -n index.ndx -o posre.itp -fc 1000 1000 1000
+awk '/\[ system \]/{print "#ifdef POSRES\n#include \"posre.itp\"\n#endif"}1' topol.top > topol_new.top
+
+gmx grompp  -c EM100.gro -f NPT100p.mdp -o NPT100.tpr -r EM100.gro -p topol_new.top -maxwarn 1
+gmx mdrun -nt 16 -s NPT100_new.tpr -deffnm NPT100_new -c NPT100_new.gro
+
+gmx grompp  -c NPT100_new.gro -f NPT100p.mdp -o NPT100.tpr -r NPT100_new.gro -p topol.top -maxwarn 1
 gmx mdrun -nt 16 -s NPT100.tpr -deffnm NPT100 -c NPT100.gro
+
+
 
 gmx grompp  -c boxxx.pdb -f EM.mdp -o EM.tpr -r boxxx.pdb -p topol.top
 gmx mdrun -nt 16 -s EM.tpr -deffnm EM -c EM.gro
-gmx grompp  -c EM.gro -f NPTP.mdp -o NPT.tpr -r EM.gro -p topol.top
+echo -e "a  bb\nq" | gmx make_ndx -f EM.gro -o index.ndx
+echo -e "bb"|gmx genrestr -f EM.gro -n index.ndx -o posre.itp -fc 1000 1000 1000
+awk '/\[ system \]/{print "#ifdef POSRES\n#include \"posre.itp\"\n#endif"}1' topol.top > topol_new.top
+
+gmx grompp  -c EM.gro -f NPTP.mdp -o NPT.tpr -r EM.gro -p topol_new.top -maxwarn 1
+gmx mdrun -nt 16 -s NPT_new.tpr -deffnm NPT_new -c NPT_new.gro
+
+gmx grompp  -c NPT_new.gro -f NPTP.mdp -o NPT.tpr -r NPT_new.gro -p topol.top -maxwarn 1
 gmx mdrun -nt 16 -s NPT.tpr -deffnm NPT -c NPT.gro
 
 mkdir BR
@@ -64,12 +80,26 @@ cp ../../../../MDP/peptides/NPTB100.mdp .
 
 gmx grompp  -c ../boxxx.pdb -f EM100.mdp -o EM100.tpr -r ../boxxx.pdb -p ../topol.top
 gmx mdrun -nt 16 -s EM100.tpr -deffnm EM100 -c EM100.gro
-gmx grompp  -c EM100.gro -f NPTB100.mdp -o NPT100.tpr -r EM100.gro -p ../topol.top
+echo -e "a  bb\nq" | gmx make_ndx -f EM.gro -o index.ndx
+echo -e "bb"|gmx genrestr -f EM.gro -n index.ndx -o posre.itp -fc 1000 1000 1000
+awk '/\[ system \]/{print "#ifdef POSRES\n#include \"posre.itp\"\n#endif"}1' ../topol.top > topol_new.top
+
+gmx grompp  -c EM100.gro -f NPTB100.mdp -o NPT100_new.tpr -r EM100.gro -p ../topol_new.top -maxwarn 1
+gmx mdrun -nt 16 -s NPT100_new.tpr -deffnm NPT100_new -c NPT100_new.gro
+
+gmx grompp  -c NPT100_new.gro -f NPTB100.mdp -o NPT100.tpr -r NPT100_new.gro -p ../topol.top -maxwarn 1
 gmx mdrun -nt 16 -s NPT100.tpr -deffnm NPT100 -c NPT100.gro
 
 gmx grompp  -c ../boxxx.pdb -f EM.mdp -o EM.tpr -r ../boxxx.pdb -p ../topol.top
 gmx mdrun -nt 16 -s EM.tpr -deffnm EM -c EM.gro
-gmx grompp  -c EM.gro -f NPTB.mdp -o NPT.tpr -r EM.gro -p ../topol.top
+echo -e "a  bb\nq" | gmx make_ndx -f EM.gro -o index.ndx
+echo -e "bb"|gmx genrestr -f EM.gro -n index.ndx -o posre.itp -fc 1000 1000 1000
+awk '/\[ system \]/{print "#ifdef POSRES\n#include \"posre.itp\"\n#endif"}1' ../topol.top > topol_new.top
+
+gmx grompp  -c EM.gro -f NPTB.mdp -o NPT_new.tpr -r EM.gro -p ../topol_new.top -maxwarn 1
+gmx mdrun -nt 16 -s NPT_new.tpr -deffnm NPT_new -c NPT_new.gro
+
+gmx grompp  -c NPT_new.gro -f NPTB.mdp -o NPT.tpr -r NPT_new.gro -p ../topol.top -maxwarn 1
 gmx mdrun -nt 16 -s NPT.tpr -deffnm NPT -c NPT.gro
 
 """)
